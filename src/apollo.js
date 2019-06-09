7177d969052946d1ad942fc1672b3aa906e9d9d5
j4ys <j4y1302@gmail.com>
2019-06-09 12:57:26 +0530

    new https

--- src/apollo.js
+++ src/apollo.js
@@ -4,7 +4,7 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({
     credentials: "include",
-    uri: "http://52.66.235.142/graphql"
+    uri: "https://52.66.235.142/graphql"
   })
 });
 
