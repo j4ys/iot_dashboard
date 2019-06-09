3316841e3ece47c87c28cc5ddc95e8bb11e4ade9
j4ys <j4y1302@gmail.com>
2019-06-09 13:13:04 +0530

    added https agent

--- src/apollo.js
+++ src/apollo.js
@@ -1,10 +1,14 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "apollo-boost";
+import https from "https";
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({
     credentials: "include",
-    uri: "https://52.66.235.142/graphql"
+    uri: "https://52.66.235.142/graphql",
+    fetchOptions: {
+      agent: new https.Agent({ rejectUnauthorized: false })
+    }
   })
 });
 
